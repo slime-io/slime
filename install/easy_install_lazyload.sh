32189fa9d0a11264ec69d18f028b84cf79b55b89
yangdihang <ivanyangtt@zju.edu.cn>
2021-01-28 19:18:26 +0800

    [Slime Boot]:Modify the installation file

--- install/easy_install_lazyload.sh
+++ install/easy_install_lazyload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 kubectl create ns mesh-operator
-kubectl apply -f https://raw.githubusercontent.com/ydh926/slime/master/install/crds.yaml
-kubectl apply -f https://raw.githubusercontent.com/ydh926/slime/master/install/slime-boot-install.yaml
-kubectl apply -f https://raw.githubusercontent.com/ydh926/slime/master/install/config/lazyload_install.yaml --validate=false
+kubectl apply -f https://raw.githubusercontent.com/slime-io/slime/master/install/crds.yaml
+kubectl apply -f https://raw.githubusercontent.com/slime-io/slime/master/install/slime-boot-install.yaml
+kubectl apply -f https://raw.githubusercontent.com/slime-io/slime/master/install/config/lazyload_install.yaml --validate=false
