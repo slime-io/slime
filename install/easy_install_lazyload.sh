9b2603cdb854eda2811f092579883d953d3bab1f
yangdihang <ivanyangtt@zju.edu.cn>
2021-03-09 15:01:54 +0800

    update demo install script

--- install/easy_install_lazyload.sh
+++ install/easy_install_lazyload.sh
@@ -2,4 +2,4 @@
 kubectl create ns mesh-operator
 kubectl apply -f https://raw.githubusercontent.com/slime-io/slime/master/install/crds.yaml
 kubectl apply -f https://raw.githubusercontent.com/slime-io/slime/master/install/slime-boot-install.yaml
-kubectl apply -f https://raw.githubusercontent.com/slime-io/slime/master/install/config/lazyload_install.yaml --validate=false
+kubectl apply -f config/lazyload_install_with_metric.yaml --validate=false
