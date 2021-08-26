006591c188b5f5c9586a4141dc568634d320cfcd
wangchenyu07 <wangchenyu07@corp.netease.com>
2021-08-26 12:00:37 +0800

    change readme and install files

---
+++ install/samples/lazyload/easy_uninstall_lazyload.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+for i in $(kubectl get ns);do kubectl delete servicefence -n $i --all;done
+kubectl delete -f https://raw.githubusercontent.com/cywang1905/slime/v0.2.0-alpha/install/samples/lazyload/slimeboot_lazyload.yaml
+kubectl delete -f https://raw.githubusercontent.com/cywang1905/slime/v0.2.0-alpha/install/init/deployment_slime-boot.yaml
+kubectl delete -f https://raw.githubusercontent.com/cywang1905/slime/v0.2.0-alpha/install/init/crds.yaml
+kubectl delete ns mesh-operator
---
+++ install/samples/smartlimiter/easy_install_limiter.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+kubectl create ns mesh-operator
+kubectl apply -f https://raw.githubusercontent.com/cywang1905/slime/v0.2.0-alpha/install/init/crds.yaml
+kubectl apply -f https://raw.githubusercontent.com/cywang1905/slime/v0.2.0-alpha/install/init/deployment_slime-boot.yaml
+kubectl apply -f https://raw.githubusercontent.com/cywang1905/slime/v0.2.0-alpha/install/samples/smartlimiter/slimeboot_smartlimiter.yaml
