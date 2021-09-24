cf8aaeb413c3009e45856909c3357011e06f4b6e
fangzhiheng <fangzhiheng@corp.netease.com>
2021-09-24 10:56:55 +0800

    Improve publish.sh image_tag logic and add VERSION to lazyload/limiter

--- bin/publish.sh
+++ bin/publish.sh
@@ -11,13 +11,23 @@ if test -z "$MOD"; then
   fatal "empty MOD"
 fi
 
+version=$(cat VERSION)  # get version from file
 commit=$(git rev-parse --short HEAD)
-tag=$(git show-ref --tags| grep "$commit" | awk -F"[/]" '{print $3}')
-if [ -z "$tag" ]; then
-  branch=$(git symbolic-ref --short -q HEAD)
-  image_tag="$branch-$commit"
+if [[ -z "${version}" ]]; then
+  tag=$(git show-ref --tags| grep "$commit" | awk -F"[/]" '{print $3}')
+  if [[ -z "${tag}" ]]; then
+    branch=$(git symbolic-ref --short -q HEAD)
+    if [[ -n "${branch}" ]]; then
+      version=$branch
+    fi
+  else
+    version=$tag  # use HEAD tag as version
+  fi
+fi
+if [ -z "$version" ]; then
+  image_tag="$commit"
 else
-  image_tag="$tag"
+  image_tag="$version-$commit"
 fi
 
 image_url="$HUB/slime-$MOD:$image_tag"
@@ -37,6 +47,18 @@ fi
 export GOOS=linux
 export GOARCH=amd64
 
+function print_info() {
+  for info in "$@"; do
+    case "$info" in
+    image)
+      echo -e "image:\n  image_url: ${image_url}"
+      ;;
+    *)
+      echo "unknown info: ${info}" >&2
+    esac
+  done
+}
+
 set -x
 for action in $actions; do
   case "$action" in
@@ -45,6 +67,13 @@ for action in $actions; do
     ;;
   image)
     docker build -t "${image_url}" .
+    ;;
+  print)  # should be the only action
+    # rest param will be consider as info to print, like: *.sh print image
+    set +x
+    shift
+    print_info "$@"
+    break
     ;;
   push)
     docker push "${image_url}"
