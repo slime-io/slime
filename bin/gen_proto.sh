8e87895e5f10f2408ca920a0bcbb7dec34746c47
yonka <yonka.fang@foxmail.com>
2021-11-15 01:27:38 +0800

    Move Timestamp to framework/apis/common/v1alpha1/types.proto

--- bin/gen_proto.sh
+++ bin/gen_proto.sh
@@ -1,4 +1,13 @@
+# sudo apt install protobuf-compiler
+# GO111MODULE=off go get github.com/gogo/protobuf/proto
+
+GOPATH=${GOPATH:-$(go env GOPATH)}
 for f in "$@"; do
   d=$(dirname $f)
-  protoc -I="$d" -I="$(dirname $0)/../framework" -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/protobuf --gogo_opt=paths=source_relative --gogo_opt=Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types --gogo_out="$d" "$f"
+  protoc -I="$d" -I="$(dirname $0)/../framework" \
+    -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/protobuf \
+    --gogo_opt=paths=source_relative \
+    --gogo_opt=Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types \
+    --gogo_opt=Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types \
+    --gogo_out="$d" "$f"
 done
