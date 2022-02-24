cd35c65a50a52478322ef938b06df2e7db5f65d9
mouceL <liu892681141@163.com>
2022-02-24 19:55:18 +0800

    add missing function: push to all hubs

--- bin/publish.sh
+++ bin/publish.sh
@@ -3,11 +3,30 @@
 # usage
 # `sh ./publish.sh ALL` in each submodules
 
-HUB=${HUB:-"docker.io/liuliluo"}
+HUB=${HUB:-"docker.io/slimeio registry.cn-hangzhou.aliyuncs.com/slimeio"}
+PUSH_HUBS="$HUB"
+first_hub=`echo $HUB|awk -F " " '{print $1}'`
 TARGET_GOARCH=${TARGET_GOARCH:-${GOARCH:-amd64}}
 TARGET_GOOS=${TARGET_GOOS:-${GOOS:-linux}}
 CGO_ENABLED=${CGO_ENABLED:-0}
 export GO111MODULE=on
+
+function fatal() {
+  echo "$1" >&2
+  exit 1
+}
+
+function calc_unstaged_hash() {
+    local tmp_f
+    tmp_f=`mktemp`
+    cp .git/index "$tmp_f"
+    GIT_INDEX_FILE="$tmp_f" git add -u
+    GIT_INDEX_FILE="$tmp_f" git write-tree
+}
+
+if test -z "$MOD"; then
+  fatal "empty MOD"
+fi
 
 if [[ -z "$TARGET_GOOS" ]]; then
   if uname | grep -q Darwin; then
@@ -20,6 +39,13 @@ fi
 if test -z "$MOD"; then
   fatal "empty MOD"
 fi
+
+dirty=
+if [[ -z "${IGNORE_DIRTY}" && -n "$(git status -s --porcelain)" ]]; then
+  unstaged_hash=$(calc_unstaged_hash)
+  dirty="-dirty_${unstaged_hash::7}"
+fi
+
 
 if [[ -z "$TAG" ]]; then
   branch=$(git symbolic-ref --short -q HEAD)
@@ -40,16 +66,16 @@ if [[ -z "$TAG" ]]; then
   TAG=${TAG}_${TARGET_GOOS}_${TARGET_GOARCH}
 
   if test -z "${IGNORE_DIRTY}" && test -n "$(git status -s --porcelain)"; then
-    TAG=$TAG-dirty
-    TAG_NO_ARCH=$TAG_NO_ARCH-dirty
+    TAG=$TAG${dirty}
+    TAG_NO_ARCH=$TAG_NO_ARCH${dirty}
   fi
 else
   TAG_NO_ARCH=$TAG
 fi
 
-image="${HUB}/$MOD:${TAG}"
-image_no_arch="${HUB}/$MOD:${TAG_NO_ARCH}"
-
+image="${first_hub}/$MOD:${TAG}"
+image_no_arch="${first_hub}/$MOD:${TAG_NO_ARCH}"
+image_url=${image}
 ALL_ACTIONS=${ALL_ACTIONS:-"build image image-push"}
 
 actions=
@@ -77,8 +103,14 @@ for action in $actions; do
     fi
     ;;
   image-push)
-    echo "push image $image"
-    docker push $image
+    for push_hub in ${PUSH_HUBS}; do
+      push_url="${push_hub}/$MOD:${TAG}"
+      if [[ "${push_url}" != "${image_url}" ]]; then
+        docker tag "${image_url}" "${push_url}"
+      fi
+      echo "push image $push_url"
+      docker push "${push_url}"
+    done
     ;;
   print-image)  # should be the only action
     echo "$image"
