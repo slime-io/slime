c70ae0c96b3e91a2959fdce72bc8327e00feabfb
cywang1905 <cywang1905@163.com>
2022-03-17 17:18:40 +0800

    Support for choosing different envoy filter names

--- framework/util/const.go
+++ framework/util/const.go
@@ -1,15 +1,49 @@
 package util
 
+import (
+	"os"
+)
+
+var (
+	Envoy_Ratelimit             string
+	Envoy_Route                 string
+	Envoy_HttpConnectionManager string
+)
+
+func init() {
+	if os.Getenv("ENVOY_FILTER_NAME_LEGACY") != "" {
+		Envoy_Ratelimit = Envoy_Ratelimit_v1
+		Envoy_Route = Envoy_Route_v1
+		Envoy_HttpConnectionManager = Envoy_HttpConnectionManager_v1
+	} else {
+		Envoy_Ratelimit = Envoy_Ratelimit_v2
+		Envoy_Route = Envoy_Route_v2
+		Envoy_HttpConnectionManager = Envoy_HttpConnectionManager_v2
+	}
+}
+
+// v1
 const (
 	// Plugins
-	Envoy_Ratelimit             = "envoy.ratelimit"
-	Envoy_Cors                  = "envoy.cors"
-	Envoy_FilterHttpWasm        = "envoy.filters.http.wasm"
-	Envoy_WasmV8                = "envoy.wasm.runtime.v8"
-	Envoy_Route                 = "envoy.router"
-	Envoy_HttpConnectionManager = "envoy.http_connection_manager"
-	Envoy_LocalRateLimit        = "envoy.filters.http.local_ratelimit"
-	Netease_LocalFlowControl    = "com.netease.local_flow_control"
+	Envoy_Ratelimit_v1             = "envoy.ratelimit"
+	Envoy_Route_v1                 = "envoy.router"
+	Envoy_HttpConnectionManager_v1 = "envoy.http_connection_manager"
+)
+
+// v2
+const (
+	// Plugins
+	Envoy_Ratelimit_v2             = "envoy.filters.network.ratelimit"
+	Envoy_Route_v2                 = "envoy.filters.http.router"
+	Envoy_HttpConnectionManager_v2 = "envoy.filters.network.http_connection_manager"
+)
+
+const (
+	Envoy_Cors               = "envoy.cors"
+	Envoy_FilterHttpWasm     = "envoy.filters.http.wasm"
+	Envoy_WasmV8             = "envoy.wasm.runtime.v8"
+	Envoy_LocalRateLimit     = "envoy.filters.http.local_ratelimit"
+	Netease_LocalFlowControl = "com.netease.local_flow_control"
 
 	Struct_Wasm_Config        = "config"
 	Struct_Wasm_Name          = "name"
