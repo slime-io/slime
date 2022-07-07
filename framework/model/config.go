3ccf0e94897020560080f560142a3874580093cb
believening <kang_417@yeah.net>
2022-07-07 15:10:16 +0800

    framework: support passing labels to children cr
    Signed-off-by: believening <kang_417@yeah.net>

--- framework/model/config.go
+++ framework/model/config.go
@@ -1,6 +1,15 @@
 package model
 
+import (
+	"os"
+	"regexp"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
 const IstioRevLabel = "istio.io/rev"
+
+var passthroughLabelPattern = regexp.MustCompile(os.Getenv("SLIME_PASS_LBL_REG"))
 
 func IstioRevFromLabel(l map[string]string) string {
 	if l == nil {
@@ -31,4 +40,31 @@ func PatchIstioRevLabel(lbls *map[string]string, rev string) {
 	}
 
 	(*lbls)[IstioRevLabel] = rev
+}
+
+func PassthroughLabels(lbls *map[string]string, from map[string]string) {
+	if passthroughLabelPattern.String() == "" {
+		return
+	}
+	for k, v := range from {
+		if !passthroughLabelPattern.MatchString(k) {
+			continue
+		}
+		if *lbls == nil {
+			*lbls = map[string]string{}
+		}
+		(*lbls)[k] = v
+	}
+}
+
+func PatchObjectMeta(dst, src *metav1.ObjectMeta) {
+	if src == nil {
+		return
+	}
+
+	if dst == nil {
+		*dst = metav1.ObjectMeta{}
+	}
+
+	PassthroughLabels(&dst.Labels, src.Labels)
 }
