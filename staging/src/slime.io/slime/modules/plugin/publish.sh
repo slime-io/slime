4885815d3ee6fdacb96fe6ef11282a9529cb4302
MouceL <liuliluo@corp.netease.com>
2022-11-18 10:52:15 +0800

    add BASE_IMAGE to change base image

--- staging/src/slime.io/slime/modules/plugin/publish.sh
+++ staging/src/slime.io/slime/modules/plugin/publish.sh
@@ -2,7 +2,7 @@
 
 source *.env.sh 2>/dev/null
 export MOD=plugin
-if [[ "$1" == "publish" ]]; then
+if [[ "$1" == "publish" || "$1" == "copy"  ]]; then
   ../../../../../../bin/multiarch.sh "$@"
 else
   ../../../../../../bin/publish.sh "$@"
