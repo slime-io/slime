a6023e72f4d19cc59a4de3412e650db22d6131da
fangzhiheng <fangzhiheng@corp.netease.com>
2022-12-09 10:10:31 +0800

    meshregistry: use standard Dockerfile and publish.sh

--- staging/src/slime.io/slime/modules/meshregistry/publish.sh
+++ staging/src/slime.io/slime/modules/meshregistry/publish.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 export MOD=meshregistry
-../../../../../../bin/publish.sh "$@"
+
+if [[ "$1" == "publish" || "$1" == "copy"  ]]; then
+  ../../../../../../bin/multiarch.sh "$@"
+else
+  ../../../../../../bin/publish.sh "$@"
+fi
