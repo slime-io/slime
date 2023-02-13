c8014095fed5d35979a1f4d06688ac16b4102ed9
MouceL <liuliluo@corp.netease.com>
2023-02-13 19:51:03 +0800

    lazyload: registry se eventhandler

--- staging/src/slime.io/slime/modules/lazyload/controllers/serviceentry_cache.go
+++ staging/src/slime.io/slime/modules/lazyload/controllers/serviceentry_cache.go
@@ -1,26 +1,29 @@
 package controllers
 
 import (
-	"context"
 	"slime.io/slime/framework/bootstrap"
 	"slime.io/slime/framework/bootstrap/resource"
 	"slime.io/slime/framework/bootstrap/serviceregistry/model"
 	"slime.io/slime/framework/bootstrap/serviceregistry/serviceentry"
 )
 
-func (r *ServicefenceReconciler) registerSeHandler(ctx context.Context) {
-	log := log.WithField("function", "StartSeCache")
-	seTocache := func(old resource.Config, cfg resource.Config, e bootstrap.Event) {
+func (r *ServicefenceReconciler) RegisterSeHandler() {
+
+	// we only increase, not delete port
+	sePortToCache := func(old resource.Config, cfg resource.Config, e bootstrap.Event) {
 		switch e {
 		case bootstrap.EventAdd:
 			istioSvcs, _ := serviceentry.ConvertSvcsAndEps(cfg, "")
-			log.Infof("get istioSvc %+v", istioSvcs)
+			r.cachePort(istioSvcs)
+		case bootstrap.EventUpdate:
+			istioSvcs, _ := serviceentry.ConvertSvcsAndEps(cfg, "")
 			r.cachePort(istioSvcs)
 		default:
 		}
 	}
 	log.Infof("lazyload: register serviceEntry handler")
-	r.env.ConfigController.RegisterEventHandler(resource.IstioService, seTocache)
+
+	r.env.ConfigController.RegisterEventHandler(resource.IstioService, sePortToCache)
 }
 
 func (r *ServicefenceReconciler) cachePort(istioSvcs []*model.Service) {
@@ -35,8 +38,8 @@ func (r *ServicefenceReconciler) cachePort(istioSvcs []*model.Service) {
 			if _, ok := r.portProtocolCache.Data[p]; !ok {
 				r.portProtocolCache.Data[p] = make(map[Protocol]uint)
 			}
-			// decode port name
 			r.portProtocolCache.Data[p][ProtocolHTTP]++
+			log.Debugf("get serviceentry http port %d", p)
 			r.portProtocolCache.Unlock()
 		}
 	}
