a38ffca890594dc90a25e0830c016d4650a12231
believening <kang_417@yeah.net>
2023-12-07 17:16:15 +0800

    meshreg: fix not updated  when scaling instances to zero

--- staging/src/slime.io/slime/modules/meshregistry/pkg/source/nacos/polling.go
+++ staging/src/slime.io/slime/modules/meshregistry/pkg/source/nacos/polling.go
@@ -67,15 +67,16 @@ func (s *Source) updateServiceInfo() error {
 
 	cache := s.cacheShallowCopy()
 	seMetaModifierFactory := s.getSeMetaModifierFactory()
-	for service, oldEntry := range cache {
+	for service, se := range cache {
 		if _, ok := newServiceEntryMap[service]; !ok {
 			// DELETE ==> set ep size to zero
-			oldEntryCopy := *oldEntry
-			oldEntryCopy.Endpoints = make([]*networking.WorkloadEntry, 0)
-			newServiceEntryMap[service] = &oldEntryCopy
-			event, err := buildEvent(event.Updated, oldEntry, service, s.args.ResourceNs, seMetaModifierFactory(service))
+			seCopy := *se
+			seCopy.Endpoints = make([]*networking.WorkloadEntry, 0)
+			newServiceEntryMap[service] = &seCopy
+			se = &seCopy
+			event, err := buildEvent(event.Updated, se, service, s.args.ResourceNs, seMetaModifierFactory(service))
 			if err == nil {
-				log.Infof("delete(update) nacos se, hosts: %s ,ep: %s ,size : %d ", oldEntry.Hosts[0], printEps(oldEntry.Endpoints), len(oldEntry.Endpoints))
+				log.Infof("delete(update) nacos se, hosts: %s ,ep: %s ,size : %d ", se.Hosts[0], printEps(se.Endpoints), len(se.Endpoints))
 				for _, h := range s.handlers {
 					h.Handle(event)
 				}
