89e299651bed37470b30bc3b544c991fd3b13a86
believening <kang_417@yeah.net>
2024-04-22 18:13:43 +0800

    meshreg: rewritten cache implementations with generic

--- staging/src/slime.io/slime/modules/meshregistry/pkg/util/cache/nodeCache.go
+++ staging/src/slime.io/slime/modules/meshregistry/pkg/util/cache/nodeCache.go
@@ -1,9 +1,6 @@
 package cache
 
 import (
-	"sync"
-
-	cmap "github.com/orcaman/concurrent-map/v2"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/cache"
@@ -11,146 +8,39 @@ import (
 	"slime.io/slime/modules/meshregistry/pkg/multicluster"
 )
 
-var K8sNodeCaches = &nodeCacheHandler{}
+var K8sNodeCaches = &cacheHandler[*metav1.ObjectMeta, *v1.Node]{
+	objectHandler: nodeHandler{},
+}
 
-func newNodeCache() *nodeCache {
-	nc := &nodeCache{
-		cache: cmap.New[*metav1.ObjectMeta](),
+type nodeHandler struct{}
+
+func (h nodeHandler) informer(cluster *multicluster.Cluster) cache.SharedIndexInformer {
+	return cluster.KubeInformer.Core().V1().Nodes().Informer()
+}
+
+func (h nodeHandler) meta(meta *metav1.ObjectMeta) *metav1.ObjectMeta {
+	return meta
+}
+
+func (h nodeHandler) hostKey(meta *metav1.ObjectMeta) string {
+	return meta.Name
+}
+
+func (h nodeHandler) onAdd(n *v1.Node) (string, *metav1.ObjectMeta, bool) {
+	return h.onUpdate(nil, n)
+}
+
+func (h nodeHandler) onUpdate(_, n *v1.Node) (string, *metav1.ObjectMeta, bool) {
+	if n != nil {
+		n.ObjectMeta.ManagedFields = nil // ignore managed fields
+		return n.Name, &n.ObjectMeta, true
 	}
-	nc.Handler = cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			nc.add(obj)
-		},
-		UpdateFunc: func(oldObj, newObj interface{}) {
-			nc.update(oldObj, newObj)
-		},
-		DeleteFunc: func(obj interface{}) {
-			nc.delete(obj)
-		},
-	}
-	return nc
+	return "", nil, false
 }
 
-type nodeCache struct {
-	cache     cmap.ConcurrentMap[string, *metav1.ObjectMeta]
-	Handler   cache.ResourceEventHandlerFuncs
-	hasSynced func() bool
-}
-
-func (nc *nodeCache) GetAll() cmap.ConcurrentMap[string, *metav1.ObjectMeta] {
-	return nc.cache
-}
-
-func (nc *nodeCache) Get(ip string) (meta *metav1.ObjectMeta, exist bool) {
-	value, exist := nc.cache.Get(ip)
-	if exist {
-		return value, exist
-	}
-	return nil, false
-}
-
-func (nc *nodeCache) GetHostKey(key string) (string, bool) {
-	_, exist := nc.cache.Get(key)
-	return key, exist
-}
-
-func (nc *nodeCache) HasSynced() bool {
-	return nc.hasSynced()
-}
-
-func (nc *nodeCache) add(obj interface{}) {
-	node, ok := obj.(*v1.Node)
-	if ok {
-		nc.cache.Set(node.Name, &node.ObjectMeta)
-	}
-}
-
-func (nc *nodeCache) update(oldObj, newObj interface{}) {
-	node, ok := newObj.(*v1.Node)
-	if ok {
-		nc.cache.Set(node.Name, &node.ObjectMeta)
-	}
-}
-
-func (nc *nodeCache) delete(obj interface{}) {
-	node, ok := obj.(*v1.Node)
-	if ok {
-		nc.cache.Remove(node.Name)
-	}
-}
-
-type nodeCacheHandler struct {
-	sync.Mutex
-	caches caches[*metav1.ObjectMeta]
-}
-
-func (nch *nodeCacheHandler) GetAll() map[string]cmap.ConcurrentMap[string, *metav1.ObjectMeta] {
-	nch.Lock()
-	defer nch.Unlock()
-	return nch.caches.Get()
-}
-
-// Note: Use IP as cache key in single cluster, this interface does not work in multi-cluster multi-network environments
-func (nch *nodeCacheHandler) Get(ip string) (meta *metav1.ObjectMeta, exist bool) {
-	nch.Lock()
-	defer nch.Unlock()
-	for _, nodes := range nch.caches {
-		meta, exist := nodes.Get(ip)
-		if exist {
-			return meta, exist
-		}
-	}
-	return nil, false
-}
-
-func (nch *nodeCacheHandler) GetHostKey(ip string) (string, bool) {
-	nch.Lock()
-	defer nch.Unlock()
-	for _, nodes := range nch.caches {
-		host, exist := nodes.GetHostKey(ip)
-		if exist {
-			return host, exist
-		}
+func (h nodeHandler) onDelete(n *v1.Node) (string, bool) {
+	if n != nil {
+		return n.Name, true
 	}
 	return "", false
-}
-
-func (nch *nodeCacheHandler) ClusterAdded(cluster *multicluster.Cluster, stopCh <-chan struct{}) error {
-	nodeInformer := cluster.KubeInformer.Core().V1().Nodes().Informer()
-	nch.Lock()
-	defer nch.Unlock()
-	if nch.caches == nil {
-		nch.caches = make(map[string]objectCache[*metav1.ObjectMeta])
-	}
-	nodeCache := newNodeCache()
-	nodeCache.hasSynced = nodeInformer.HasSynced
-	nch.caches[cluster.ID] = nodeCache
-	nodeInformer.AddEventHandler(nodeCache.Handler)
-	go nodeInformer.Run(stopCh)
-	return nil
-}
-
-func (nch *nodeCacheHandler) ClusterUpdated(cluster *multicluster.Cluster, clusterStopCh <-chan struct{}) error {
-	if err := nch.ClusterDeleted(cluster.ID); err != nil {
-		return err
-	}
-	return nch.ClusterAdded(cluster, clusterStopCh)
-}
-
-func (nch *nodeCacheHandler) ClusterDeleted(clusterID string) error {
-	nch.Lock()
-	defer nch.Unlock()
-	delete(nch.caches, clusterID)
-	return nil
-}
-
-func (nch *nodeCacheHandler) HasSynced() bool {
-	nch.Lock()
-	defer nch.Unlock()
-	for _, c := range nch.caches {
-		if !c.HasSynced() {
-			return false
-		}
-	}
-	return true
 }
--- staging/src/slime.io/slime/modules/meshregistry/pkg/util/cache/podCache.go
+++ staging/src/slime.io/slime/modules/meshregistry/pkg/util/cache/podCache.go
@@ -1,9 +1,6 @@
 package cache
 
 import (
-	"sync"
-
-	cmap "github.com/orcaman/concurrent-map/v2"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/cache"
@@ -11,24 +8,8 @@ import (
 	"slime.io/slime/modules/meshregistry/pkg/multicluster"
 )
 
-var K8sPodCaches = &podCacheHandler{}
-
-func newPodCache() *podCache {
-	pc := &podCache{
-		cache: cmap.New[*podWrapper](),
-	}
-	pc.Handler = cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			pc.add(obj)
-		},
-		UpdateFunc: func(oldObj, newObj interface{}) {
-			pc.update(oldObj, newObj)
-		},
-		DeleteFunc: func(obj interface{}) {
-			pc.delete(obj)
-		},
-	}
-	return pc
+var K8sPodCaches = &cacheHandler[*podWrapper, *v1.Pod]{
+	objectHandler: podCacheHandler{},
 }
 
 type podWrapper struct {
@@ -36,153 +17,46 @@ type podWrapper struct {
 	NodeName string
 }
 
-type podCache struct {
-	cache     cmap.ConcurrentMap[string, *podWrapper]
-	Handler   cache.ResourceEventHandlerFuncs
-	hasSynced func() bool
+type podCacheHandler struct{}
+
+func (h podCacheHandler) informer(cluster *multicluster.Cluster) cache.SharedIndexInformer {
+	return cluster.KubeInformer.Core().V1().Pods().Informer()
 }
 
-func (pc *podCache) GetAll() cmap.ConcurrentMap[string, *podWrapper] {
-	return pc.cache
+func (h podCacheHandler) meta(pw *podWrapper) *metav1.ObjectMeta {
+	return pw.Meta
 }
 
-func (pc *podCache) Get(ip string) (meta *metav1.ObjectMeta, exist bool) {
-	value, exist := pc.cache.Get(ip)
-	if exist {
-		return value.Meta, exist
+func (h podCacheHandler) hostKey(pw *podWrapper) string {
+	return pw.NodeName
+}
+
+func (h podCacheHandler) onAdd(p *v1.Pod) (string, *podWrapper, bool) {
+	return h.onUpdate(nil, p)
+}
+
+func (h podCacheHandler) onUpdate(_, p *v1.Pod) (string, *podWrapper, bool) {
+	if p == nil {
+		return "", nil, false
 	}
-	return nil, false
-}
-
-func (pc *podCache) GetHostKey(ip string) (string, bool) {
-	value, exist := pc.cache.Get(ip)
-	if exist {
-		return value.NodeName, true
+	ip := p.Status.PodIP
+	if ip == "" {
+		return "", nil, false
 	}
-	return "", false
-}
-
-func (pc *podCache) HasSynced() bool {
-	return pc.hasSynced()
-}
-
-func (pc *podCache) add(obj interface{}) {
-	pod, ok := obj.(*v1.Pod)
-	if ok {
-		ip := pod.Status.PodIP
-		if ip == "" {
-			// log.Warnf("pod %s/%s has no ip when add", pod.Namespace, pod.Name)
-			return
-		}
-		if pod.Status.Phase != v1.PodRunning {
-			// log.Warnf("pod %s/%s is not running when add", pod.Namespace, pod.Name)
-			return
-		}
-		pc.cache.Set(ip, &podWrapper{&pod.ObjectMeta, pod.Spec.NodeName})
+	if p.Status.Phase != v1.PodRunning {
+		return "", nil, false
 	}
+	p.ObjectMeta.ManagedFields = nil // ignore managed fields
+	return ip, &podWrapper{&p.ObjectMeta, p.Spec.NodeName}, true
 }
 
-func (pc *podCache) update(oldObj, newObj interface{}) {
-	pod, ok := newObj.(*v1.Pod)
-	if ok {
-		ip := pod.Status.PodIP
-		if ip == "" {
-			// log.Warnf("pod %s/%s has no ip when update", pod.Namespace, pod.Name)
-			return
-		}
-		if pod.Status.Phase != v1.PodRunning {
-			// log.Warnf("pod %s/%s is not running when update", pod.Namespace, pod.Name)
-			return
-		}
-		pc.cache.Set(ip, &podWrapper{&pod.ObjectMeta, pod.Spec.NodeName})
+func (h podCacheHandler) onDelete(p *v1.Pod) (string, bool) {
+	if p == nil {
+		return "", false
 	}
-}
-
-func (pc *podCache) delete(obj interface{}) {
-	pod, ok := obj.(*v1.Pod)
-	if ok {
-		ip := pod.Status.PodIP
-		if ip == "" {
-			// log.Warnf("pod %s/%s has no ip when delete", pod.Namespace, pod.Name)
-			return
-		}
-		pc.cache.Remove(ip)
+	ip := p.Status.PodIP
+	if ip == "" {
+		return "", false
 	}
-}
-
-type podCacheHandler struct {
-	sync.Mutex
-	caches caches[*podWrapper]
-}
-
-func (pch *podCacheHandler) GetAll() map[string]cmap.ConcurrentMap[string, *podWrapper] {
-	pch.Lock()
-	defer pch.Unlock()
-	return pch.caches.Get()
-}
-
-// Note: Use IP as cache key in flat networks, this interface does not work in multi-cluster multi-network environments
-func (pch *podCacheHandler) Get(ip string) (meta *metav1.ObjectMeta, exist bool) {
-	pch.Lock()
-	defer pch.Unlock()
-	for _, pods := range pch.caches {
-		meta, exist := pods.Get(ip)
-		if exist {
-			return meta, exist
-		}
-	}
-	return nil, false
-}
-
-// Note: Use IP as cache key in flat networks, this interface does not work in multi-cluster multi-network environments
-func (pch *podCacheHandler) GetHostKey(ip string) (string, bool) {
-	pch.Lock()
-	defer pch.Unlock()
-	for _, pods := range pch.caches {
-		host, exist := pods.GetHostKey(ip)
-		if exist {
-			return host, exist
-		}
-	}
-	return "", false
-}
-
-func (pch *podCacheHandler) ClusterAdded(cluster *multicluster.Cluster, stopCh <-chan struct{}) error {
-	podInformer := cluster.KubeInformer.Core().V1().Pods().Informer()
-	pch.Lock()
-	defer pch.Unlock()
-	if pch.caches == nil {
-		pch.caches = make(map[string]objectCache[*podWrapper])
-	}
-	podCache := newPodCache()
-	podCache.hasSynced = podInformer.HasSynced
-	pch.caches[cluster.ID] = podCache
-	podInformer.AddEventHandler(podCache.Handler)
-	go podInformer.Run(stopCh)
-	return nil
-}
-
-func (pch *podCacheHandler) ClusterUpdated(cluster *multicluster.Cluster, clusterStopCh <-chan struct{}) error {
-	if err := pch.ClusterDeleted(cluster.ID); err != nil {
-		return err
-	}
-	return pch.ClusterAdded(cluster, clusterStopCh)
-}
-
-func (pch *podCacheHandler) ClusterDeleted(clusterID string) error {
-	pch.Lock()
-	defer pch.Unlock()
-	delete(pch.caches, clusterID)
-	return nil
-}
-
-func (pch *podCacheHandler) HasSynced() bool {
-	pch.Lock()
-	defer pch.Unlock()
-	for _, c := range pch.caches {
-		if !c.HasSynced() {
-			return false
-		}
-	}
-	return true
+	return ip, true
 }
